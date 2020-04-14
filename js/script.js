e037110dc9408712e60d05952c3a45c4bd8edd81
AnubhavMadhav <201851024@iiitvadodara.ac.in>
2020-04-14 15:45:42 +0530

    Check StackOverflow

--- js/script.js
+++ js/script.js
@@ -3,7 +3,20 @@
 //     document.getElementById('myform').submit();
 // }
 
-
+window.addEventListener("load", function() {
+    document.getElementById("mailtolink").addEventListener("click", function(e) {
+        e.preventDefault() // remove after testing
+        let url = new URL(this.href);
+        const name = document.querySelector("[name=yourName]").value.trim();
+        const email = document.querySelector("[name=yourEmail]").value.trim();
+        const message = document.querySelector("[name=yourMessage]").value.trim();
+        url.searchParams.set("from", email);
+        url.searchParams.set("subject", "Message from " + (document.title || location.hostname))
+        url.searchParams.set("body", name + " said " + message)
+        this.href = url;
+        console.log(url)
+    })
+});
 
 const index2 = document.getElementById('');
 
